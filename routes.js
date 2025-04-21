aa186978ecc092f47d87a40465a297ebbcc4dc4e
Lucas Brito <britolmbs@hotmail.com>
2025-04-21 16:11:04 -0300

    feat: Adiciona rota de contato com middleware loginRequired e formulário para criação de contatos

--- routes.js
+++ routes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('./src/controllers/homeController');
-const loginController = require('./src/controllers/loginController')
+const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middlewares');
 
 router.get('/', homeController.index);
 router.get('/login/index', loginController.index);
 router.post('/login/register', loginController.register);
-router.post('/login/login', loginController.login)
-router.get('/login/logout', loginController.logout)
+router.post('/login/login', loginController.login);
+router.get('/login/logout', loginController.logout);
+
+route.get('/contato/index',loginRequired, contatoController.index);
 
 module.exports = route;
