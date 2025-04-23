10624b55f7d343471621957d00d32df475746a6f
Lucas Brito <britolmbs@hotmail.com>
2025-04-23 17:18:23 -0300

    feat: adiciona criação e validação de contato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,7 +1,23 @@
+const Contato = require('../models/ContatoModel');
 exports.index = (req, res) => {
     res.render('contato');
 }
 
-exports.register = (req, res) => {
-   res.send(); 
+exports.register =  async (req, res) => {
+    try{
+        const contato = new Contato(req.body);
+        await contato.register();
+        if(contato.errors. length > 0) {
+          req.flash('errors', contato.errors);
+          req.session.save(() => res.redirect('back'));
+          return;
+        }
+        req.flash('success', 'Contato Criado');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+
+    } catch (e){
+       return res.render('404')
+    }
+  
 }
--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const validator = riquire('validator')
 const ContatoSchema = new mongoose.Schema({
-    titulo: { type: String, required: true },
-    descricao: String
+    nome: { type: String, required: true },
+    sobrenome: { type: String, required: false, default: '' },
+    telefone: { type: String, required: false, default: '' },
+    email: { type: String, required: false, default: '' },
+    criadoEm: { type: Date, default: Date.now}
 });
 
 const ContatoModel = mongoose.model('Contato', ContatoSchema);
@@ -10,23 +14,36 @@ function Contato(body) {
     this.body = body;
     this.errors = [];
     this.contato = null;
-}
+};
 
-contato.prototype.register = function() {
+Contato.prototype.register = async function() {
     this.valida();
+    if(this.errors.length > 0) return;
+   this.contato =  await ContatoModel.create(this.body);
+
 };
 Contato.prototype.valida = function() {
     this.cleanUp();
 
-    if(!validator.isEmail(this.body.email)) this.errors.push('Email invalido')
+    if(this.body.email && !validator.isEmail(this.body.email)) this.errors.push('Email invalido');
+    if(!this.body.nome) this.errors.push('Nome é um campo Obrigatorio');
+    if(!this.body.email && !this.body.telefone) {
+        this.errors.push('Obrigatorio email ou telefone');
 }
+};
 
-cleanUp() {
+Contato.prototype.cleanUp() = function() {
     for(const key in this.body) {
         if(typeof this.body[key] !== 'string') {
             this.body[key] = '';
         }
     }
-}
+    this.body = {
+        nome: this.body.nome,
+        sobrenome: this.body.sobrenome,
+        email: this.body.email,
+        telefone: this.body.telefone
+    }
+};
 
 module.exports = Contato;
