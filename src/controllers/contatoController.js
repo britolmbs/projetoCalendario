43517ed3d3e6881f06a9130d3f2fbdb9de0b4526
Lucas Brito <britolmbs@hotmail.com>
2025-04-26 09:56:13 -0300

    Editando contato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -34,6 +34,21 @@ exports.editIndex = async function(req, res) {
 };
 
 exports.edit = async function(req, res) {
+    try{
     if(!req.params.id) return res.render('404');
-    
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+
+    if(contato.errors.length > 0) {
+        req.flash('errors', contato.errors);
+        req.session.save(() => res.redirect('back'));
+        return;
+    }
+    req.flash('success', 'Contato Alterado!');
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+    return;
+}catch (e) {
+    res.render('404');
+}
+
 };
