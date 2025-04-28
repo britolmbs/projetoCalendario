26cbab5d5609fabefbf5691c6336f387357d6dac
Lucas Brito <britolmbs@hotmail.com>
2025-04-28 20:30:59 -0300

    validando

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,6 @@ module.exports = {
                     presets: ['@babel/env']
                 }
             }
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
         }]
     },
     devtool: 'source-map'
